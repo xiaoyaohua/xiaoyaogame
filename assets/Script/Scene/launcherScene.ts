13a96a379d54de2c0e5ea9c7cd4ace728f2765cd
zhouhua <358197995@qq.com>
2020-05-27 10:54:08 +0800

    create project

---
+++ assets/Script/Scene/launcherScene.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: hua 
+ * @Date: 2020-05-27 10:05:32 
+ * @Last Modified by: hua
+ * @Last Modified time: 2020-05-27 10:37:57
+ */
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class launcherScene extends cc.Component {
+    
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
